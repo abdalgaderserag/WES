bb33e2d73ad36bd8b9eea9febb19fbfe6f0ffd71
Abd Al-Gader Sirag <abdalgadersir@gmail.com>
2024-09-04 16:41:04 +0200

    register Auth routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,25 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+//guests routes for unauthanticated user
+Route::middleware('guest')->namespace('App\Http\Controllers')->group(function (){
+    Route::post('login','Auth\LoginController@login')->name('login');
+});
+
+
+//auth routes
+Route::middleware('auth:sanctum')->namespace('App\Http\Controllers')->group(function (){
+    Route::get('logout','Auth\LoginController@logout')->name('logout');
+
+    Route::get('u/{user}','Auth\UserController@show')->name('user');
+    Route::put('update/profile','Auth\UserController@update')->name('update_profile');
+});
+
+//admin routes
+Route::middleware('auth:sanctum')->namespace('App\Http\Controllers')->group(function (){
+    Route::post('user/register','Auth\RegisterController@register')->name('register_user');
+    Route::put('user/{user}','Auth\RegisterController@update')->name('update_user');
+    Route::get('user/{user}','Auth\RegisterController@show')->name('show_user');
+    Route::delete('user/{user}','Auth\RegisterController@delete')->name('delete_user');
+});
