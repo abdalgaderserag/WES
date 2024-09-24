742623f8137972bab0b25354a2bbb842ab750f66
Abd Al-Gader Sirag <abdalgadersir@gmail.com>
2024-09-24 12:10:45 +0200

    to last

--- resources/views/livewire/management/workers.blade.php
+++ resources/views/livewire/management/workers.blade.php
@@ -1,43 +1,48 @@
-<div id="workers">
-    @forelse($workers as $worker)
-        <div class="section" id="workers-cards">
-            <div class="item-row"></div>
-            <div class="item-row flex">
-                <img src="{{ url($worker->img) }}">
+<div class="flex" style="flex-direction: column;">
+    <input type="search" name="search" style="width: 56%;border-radius: 20px;font-size: 1.1rem;padding: 4px 20px;border:2px solid #718096" placeholder="search here"><br>
+
+    <div id="workers">
+        @forelse($workers as $worker)
+            <div class="section" id="workers-cards">
+                <div class="item-row"></div>
+                <div class="item-row flex">
+                    <img src="{{ url($worker->img) }}">
 
 
-                <div class="card-title">{{ $worker->name }}</div>
+                    <div class="card-title">{{ $worker->name }}</div>
 
 
-                <button wire:click="create({{ $worker->id }})" class="card-button">+ add task</button>
+                    <button wire:click="create({{ $worker->id }})" class="card-button">+ add task</button>
 
 
-                <div class="flex" style="flex-direction: column;margin-top: 16px">
-                    <div class="flex">
-                        <div>on-time task completion rate :</div>
-                        @php($rate = rand(70,95) . '%')
-                        <div>{{ $rate }}</div>
+                    <div class="flex" style="flex-direction: column;margin-top: 16px">
+                        <div class="flex">
+                            <div>on-time task completion rate :</div>
+                            @php($rate = rand(70,95) . '%')
+                            <div>{{ $rate }}</div>
+                        </div>
+                        {{--                    <progress value="{{ $rate }}" max="100"></progress>--}}
+                        <div style="border-radius: 8px;height: 16px;width:82%;background-color: #e4e8ed;margin-top: 16px">
+                            <div style="border-radius: 8px;height: 16px;width: {{ $rate }};background-color: #32c8a7"></div>
+                        </div>
                     </div>
-{{--                    <progress value="{{ $rate }}" max="100"></progress>--}}
-                    <div style="border-radius: 8px;height: 16px;width:82%;background-color: #e4e8ed;margin-top: 16px">
-                        <div style="border-radius: 8px;height: 16px;width: {{ $rate }};background-color: #32c8a7"></div>
-                    </div>
-                </div>
 
 
-                <div class="flex" style="font-size: 1rem;width: 96%;margin: 16px 2% 12px 0;">
-                    <div style="width: 50%;display: flex;flex-direction: column;align-items: center">
-                        <div>Active Tasks</div>
-                        <div>{{ $worker->tasks->count() }}</div>
-                    </div>
-                    <div style="width: 50%;display: flex;flex-direction: column;align-items: center">
-                        <div>completed Tasks</div>
-                        <div>{{ $worker->tasks->count() }}</div>
+                    <div class="flex" style="font-size: 1rem;width: 96%;margin: 16px 2% 12px 0;">
+                        <div style="width: 50%;display: flex;flex-direction: column;align-items: center">
+                            <div>Active Tasks</div>
+                            <div>{{ $worker->tasks->count() }}</div>
+                        </div>
+                        <div style="width: 50%;display: flex;flex-direction: column;align-items: center">
+                            <div>completed Tasks</div>
+                            <div>{{ $worker->tasks->count() }}</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    @empty
-        <h1>there is no employ in this department</h1>
-    @endforelse
+        @empty
+            <h1>there is no employ in this department</h1>
+        @endforelse
+    </div>
+
 </div>
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -5,7 +5,7 @@
         <div class="item-row">{{ $user->name }}</div>
         <div class="item-row" style="display: flex;align-items: flex-start">
             <div style="margin: 12px 2px">
-                <img src="{{ url('images/avatar.jpg') }}" style="width: 210px;border-radius: 12px">
+                <img src="{{ url($user->img) }}" style="width: 210px;border-radius: 12px">
                 <div style="margin-left: 12px">
                     <div style="color: #1a202c;font-size: 1.1rem;font-weight: 700">{{ $user->name }}</div>
                     <div style="font-size: 1rem">{{ '@' . $user->username }}</div>
