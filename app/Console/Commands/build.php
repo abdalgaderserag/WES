c185e5a3ab01870d53526025807b8f2cd72fbd48
Abd Al-Gader Sirag <abdalgadersir@gmail.com>
2024-09-26 14:58:11 +0200

    add default img

---
+++ app/Console/Commands/build.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class build extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:build';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+    }
+}
